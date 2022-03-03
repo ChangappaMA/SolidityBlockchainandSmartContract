da1f3d2e71909b4c91edb7e98c6089fb43168570
ChangappaMA <mukkatira.changappa@gmail.com>
2022-03-03 23:46:07 +0530

    Brownie Fund me project

--- demos/brownie_simple_storage/scripts/deploy.py
+++ demos/brownie_simple_storage/scripts/deploy.py
@@ -1,8 +1,10 @@
-from re import S
-from brownie import accounts, SimpleStorage
+from brownie import accounts, SimpleStorage, network, config
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def deploy_simple_storage():
-    account = accounts[0]
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
     stored_value = simple_storage.retrieve()
     transaction = simple_storage.store(15, {"from": account})
@@ -11,6 +13,12 @@ def deploy_simple_storage():
     updated = simple_storage.retrieve()
     print(updated)
 
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
