4abd33191333820569615dd79f80f737ece7ed10
ChangappaMA <mukkatira.changappa@gmail.com>
2022-03-05 00:15:41 +0530

    Mocking aggregatorV3

---
+++ demos/brownie_fund_me/scripts/fund_and_withdraw.py
@@ -0,0 +1,12 @@
+from importlib.metadata import entry_points
+from brownie import FundMe
+from scripts.helpful_scripts import get_account
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(entrance_fee)
+
+def main():
+    fund()
--- demos/brownie_fund_me/scripts/helpful_scripts.py
+++ demos/brownie_fund_me/scripts/helpful_scripts.py
@@ -1,10 +1,23 @@
-from brownie import accounts, config, network
+from brownie import accounts, config, network, MockV3Aggregator
 from dotenv import load_dotenv
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+DECIMAL = 8
+STARTING_PRICE = 20000000000
 
 load_dotenv()
 
 def get_account():
-    if network.show_active() == 'development':
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+def deploy_mocks():
+    print(f"The active network is: {network.show_active()}")
+    print("Deploying Mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(DECIMAL, STARTING_PRICE,
+                {"from": get_account()} )
+        
+    print("Mocks deployed!!")
